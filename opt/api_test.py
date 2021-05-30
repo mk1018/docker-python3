45615d9bc86c035ac2cc69d0658cca2d0dd14797
mk1018 <ktmn1018@yahoo.co.jp>
2021-05-30 15:00:35 +0900

    jsonテスト

--- opt/api_test.py
+++ opt/api_test.py
@@ -1,4 +1,5 @@
 import requests
+import json
 
 baseUrl = 'https://openapi.biki.com/'
 url = baseUrl + 'open/api/get_ticker?symbol=xqcbtc'
@@ -6,6 +7,10 @@ url = baseUrl + 'open/api/get_ticker?symbol=xqcbtc'
 response = requests.get(url)
 
 print(response.text)
+
+resJson = json.loads(response.text)
+print(resJson['data']['last'])
+# print(response.text)
 # <Response [200]> 200は成功
 
 print(type(response))
--- opt/deamon_sample.py
+++ opt/deamon_sample.py
@@ -1,13 +1,14 @@
 import sys
 import time
 import daemon
+import api_test as test
 
 def count_up():
     count = 0
     while True:
         print(count)
         count += 1
-        time.sleep(1)
+        time.sleep(5)
 
 dc = daemon.DaemonContext(stdout=sys.stdout)
 
